86c3f646d8d92886c9970f7ee9987c338fbcd658
limeechian <133497135+limeechian@users.noreply.github.com>
2024-05-13 14:43:14 +0800

    Create ClientsInfo.cs

---
+++ Models/ClientsInfo.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelRoomManagementApp.Models
+{
+    public class ClientsInfo
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string? ClientName { get; set; }
+
+        [Required]
+        //[RegularExpression(@"^[0-9-]*$", ErrorMessage = "Please enter a valid value containing only numeric digits and '-' character.")]
+        public string? ClientIcPassport { get; set; }
+
+        [Required]
+        //[RegularExpression(@"^[0-9-]*$", ErrorMessage = "Please enter a valid value containing only numeric digits and '-' character.")]
+        public string? ClientPhoneNumber { get; set; }
+
+        [Required]
+        //[RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$", ErrorMessage = "Please enter a valid email address.")]
+        public string? ClientEmail { get; set; }
+        [Required]
+        public string? ClientGender { get; set; }
+
+        [Required]
+        //[DisplayFormat(DataFormatString = "{0:M/d/yyyy}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> ClientBirthDate { get; set; }
+
+        [Required]
+        public string? ClientAddress { get; set; }
+
+        public Nullable<System.DateTime> CreatedAt { get; set; }
+
+        public Nullable<long> CreatedBy { get; set; }
+
+        public Nullable<System.DateTime> ModifiedAt { get; set; }
+
+        public Nullable<long> ModifiedBy { get; set; }
+
+        public short Status { get; set; }
+    }
+}
